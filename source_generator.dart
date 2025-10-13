11dbbaf3e6eb23f4fe997411f264d73eb70b945e
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2025-10-12 23:45:46 -0500

    Update source_generator.dart

--- source_generator.dart
+++ source_generator.dart
@@ -69,7 +69,7 @@ List<Source> _searchJsSources(Directory dir) {
                 ..appMinVerReq =
                     sourceJson["appMinVerReq"] ?? defaultSource.appMinVerReq
                 ..sourceCodeUrl =
-                    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/javascript/${sourceJson["pkgPath"] ?? sourceJson["pkgName"]}";
+                    "https://raw.githubusercontent.com/tympanicblock61/mangayomi-extensions/$branchName/javascript/${sourceJson["pkgPath"] ?? sourceJson["pkgName"]}";
               if (sourceJson["id"] != null) {
                 source = source..id = int.tryParse("${sourceJson["id"]}");
               }
